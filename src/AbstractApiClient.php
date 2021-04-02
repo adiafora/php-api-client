f25759a6587cd23805bab84b3a64dbbd9b5b28f0
Sergey Zhidkov <53189193+adiafora@users.noreply.github.com>
2021-04-02 14:41:50 +0300

    delete file() method

--- src/AbstractApiClient.php
+++ src/AbstractApiClient.php
@@ -36,19 +36,12 @@ abstract class AbstractApiClient
     protected $context;
 
     /**
-     * Name of the file, if requested.
-     *
-     * @var null|string
-     */
-    protected $fileName = null;
-
-    /**
      * AbstractApiClient constructor.
      *
      * @param string $method
      * @param array  $params
      *
-     * @throws \Exception
+     * @throws NotFoundMethodApiException
      */
     public function __construct(string $method = Method::GET, array $params = [])
     {
@@ -80,23 +73,6 @@ abstract class AbstractApiClient
     abstract public function getUrl(): string;
 
     /**
-     * If the API shall to return a file,
-     * then you need to pass the full path to the file,
-     * including the name of the file,
-     * where it will be saved.
-     *
-     * @param string $fileName
-     *
-     * @return $this
-     */
-    public function file(string $fileName)
-    {
-        $this->fileName = $fileName;
-
-        return $this;
-    }
-
-    /**
      * Main method for executing an API request.
      *
      * @return ApiResponse
@@ -120,11 +96,6 @@ abstract class AbstractApiClient
             $this->context->curlSetoptMethod($curl);
 
             $response = curl_exec($curl);
-
-            if (! is_null($this->fileName)) {
-                file_put_contents($this->fileName, $response);
-                $response = json_encode($this->fileName);
-            }
 
             $apiResponse = new ApiResponse($curl, $response, $this);
 
@@ -170,7 +141,7 @@ abstract class AbstractApiClient
      * Depending on the request method, the request context will be formed differently.
      *
      * @return AbstractContext
-     * @throws \Exception
+     * @throws NotFoundMethodApiException
      */
     protected function getContext(): AbstractContext
     {
